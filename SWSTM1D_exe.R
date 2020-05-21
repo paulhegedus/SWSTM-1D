efa0fa790af014c6ddbf7f2722e11e2430749060
paulheg <40700089+paulhegedus@users.noreply.github.com>
2020-05-20 23:42:08 -0600

    new format
    - abides by google style guide

--- SWSTM1D_exe.R
+++ SWSTM1D_exe.R
@@ -1,5 +1,6 @@
 ## SWSTM1D - Soil Water and Solute Transport Model 1D
 ## Paul Hegedus
+## May 2020
 
 ## Description:
 ## Script for loading and implementing the 1D soil transport simulation mod. 
@@ -7,76 +8,33 @@
 ## run without proper components in working directory.
 ## see flowcharts for overview  ... etc. (modSchema vX)
 ##
+## Location of modules and the model scripts can be different from inputs/
+## outputs in order to make it easier to run experiments and save inputs/
+## outputs in different folders on your computer without having to copy the
+## model script everywhere inputs and outputs are located.
 ##@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-#### Set up workspace ####
-##@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-##------------------
-## USER INPUTS
-##------------------
-# path to model files location
+
+# User Inputs ---------------------------
+# Need to provide the path to model files location (where 'modules' is)
 modPath <- "/Users/PaulBriggs/Box/Hegedus/Dissertation/Chapter3/swstm1D/dev/Code"
-# path to location of 'inputs' folder; outputs put here (does not need to be same as model)
+# Need to provide the path to location of 'inputs' folder; 
+# outputs put here (this does not need to be same as model)
 ioPath <- "/Users/PaulBriggs/Box/Hegedus/Dissertation/Chapter3/swstm1D/dev/Test"
-# string of module names
-mIn <- c("DrainModuleFC") 
-##------------------
-## source the 'sourceCode' script with non-module specific R6ClassGenerators &
-## package loading & function for checking for model requirements
-source(
-  paste0(
-    modPath,
-    "/SWSTM1D_sourceCode.R"
-  )
-)
-## check module & io (inputs/outputs) path
-checkForModelReqs(
-  modPath,
-  ioPath
-)
-##@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-#### Set up model  ####
-##@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-## 1) generate SWSTM1D class object
+# Need to provide the string of module names
+modsIn <- c("DrainModuleFC") 
+# Source Code ---------------------------
+source(paste0(modPath, "/SWSTM1D_sourceCode.R"))
+# Check Model Dependencies ---------------------------
+CheckForModelReqs(modPath,ioPath)
+# Initialize Model ---------------------------
 swstm1d <- SWSTM1D$new(
-  # path and module info
   modPath = modPath,
   ioPath = ioPath,
-  mIn = mIn
+  modsIn = modsIn
 )
-## 2) run the soil sim set up function
-swstm1d$setup() 
-##@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-#### Execute simulation ####
-##@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-## 3) execute the soil sim modules
-swstm1d$execute() 
-##@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-#### Plot/Save outputs ####
-##@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-## 4) plot and save final outputs
-swstm1d$output() 
-
-
-
-
-
-
-
-##@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-# old visuals (in folders from output() call now)
-# swstm1d$output() = swstm1d$outputter$plotFinal
-### Visualize Outputs
-soilMod1d$soilModData$plot_dpXt() # deep perc vs time
-soilMod1d$soilModData$plot_pXt() # precip vs time 
-
-#soilMod1d$soilModData$t_dat # time level data outputs 
-#soilMod1d$soilModData$z_dat # depth level data outputs
-
-for(i in 0:sim_length){
-  print(soilMod1d$soilModData$plot_vwcXz(i)) # vwc vs depth over time
-  i=Sys.sleep(2)
-}
-
-
-
-
+# SetUp Model ---------------------------
+swstm1d$SetUp() 
+# Execute Model Simulation ---------------------------
+swstm1d$Execute() 
+# Save Model Simulation Outputs ---------------------------
+swstm1d$Output() 
--- modules/DrainModuleFC.R
+++ modules/DrainModuleFC.R
@@ -1,176 +1,164 @@
 ## Title: DrainModuleFC
 ## 
-## Interface/Abstraction: This object follows the "modules" interface consisting of the methods; 
-## setUp(), execute(), update(), plotGen()
+## Interface/Abstraction: This object follows the "modules" 
+## interface consisting of the methods;SetUp(), Execute(), Update(), plotGen()
 ##
-## Description: This class is the instantaneous drain module for the 1d soil simulation model.
-## The exe method determines if precip available, and then for every layer in the 
-## class, the function calculates how much water goes in or out of each layer.
-## 
+## Description: This class is the instantaneous drain module for the 1d soil 
+## simulation model. The exe method determines if precip available, and then 
+## for every layer in the class, the function calculates how much water goes 
+## in or out of each layer.
+##
 ## Inputs: soilModData (R6 class - args: soilProfile, tDat,zDat)
-## Methods: setup, execute, update, plotGen
+## Methods: SetUp, Execute, Update, plotGen
 ##
 ##@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-#### Packages ####
-##@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-## Packages specific to this module
-# < none >
-
-##@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-#### Class  ####
-##@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+# DrainModuleFC Class Generator ---------------------------
 DrainModuleFC <- R6Class(
   classname="DrainModuleFC",
   public = list(
-    # arguments/fields
     soilModData = NULL, 
 
-    # methods
-    initialize = function(soilModData){
+    initialize = function(soilModData) {
       stopifnot(
-        # check if data structures exist
-        exists("tDat",soilModData),
-        exists("zDat",soilModData),
-        exists("ioPath",soilModData),
-                                    
-        # check for input data in 'inputs' folder
-        file.exists(paste0(soilModData$ioPath,"/inputs/DrainModuleFC_in.csv")),
-        
-        # column reqs in z dat
+        exists("tDat", soilModData),
+        exists("zDat", soilModData),
+        exists("ioPath", soilModData),
+        file.exists(paste0(soilModData$ioPath, 
+                           "/inputs/DrainModuleFC_in.csv")),
         !is.null(soilModData$zDat$vwc),
         is.numeric(soilModData$zDat$vwc), 
-        all(soilModData$zDat$vwc>0 & soilModData$zDat$vwc<1)
+        all(soilModData$zDat$vwc > 0 & soilModData$zDat$vwc < 1)
       ) 
       self$soilModData <- soilModData
     },
-    setup = function(){
-      # 1) get input data
-      dfcIn <- fread(
-        paste0(
-          self$soilModData$ioPath,
-          "/inputs/DrainModuleFC_in.csv"
-        )
-      ) %>%
+    
+    SetUp = function() {
+      # 1) Modules specific data must be in folder named 'inputs'
+      dfcIn <- fread(paste0(self$soilModData$ioPath, 
+                            "/inputs/DrainModuleFC_in.csv")) %>%
         as.data.frame()
-      
-      # check data
       stopifnot(
         is.data.frame(dfcIn),
         !is.null(dfcIn$fc),
         is.numeric(dfcIn$fc),
-        nrow(dfcIn)==nrow(self$soilModData$zDat) # length of fc must be same as layers
+        nrow(dfcIn) == nrow(self$soilModData$zDat) 
       )
-      
-      ## 2) add input data to zDat
+      # 2) Input data has to be modified
       self$soilModData$zDat$fc <- dfcIn$fc
-      
-      ## 3) add any output cols to output data
-      ## only update tDat or zDat b/c soilProfile built after
-      ## add columns to tDat
-      self$soilModData$tDat$deepPerc <- 0 # make 0 as default
-      ## add columns to zDat
-      self$soilModData$zDat$wTop <- 0 # make 0 as default
-      self$soilModData$zDat$wBot <- 0 # make 0 as default
+      # 3) Output data has to be modified (0 added as defaults to avoid elses)
+      self$soilModData$tDat$deepPerc <- 0 
+      self$soilModData$zDat$wTop <- 0 
+      self$soilModData$zDat$wBot <- 0 
     },
-    execute = function(t){
-      ## always do work on soil layers object
+    
+    Execute = function(t) {
+      # Get the number of soil layers for shorter pointer
       num_layers <- length(self$soilModData$soilProfile$soilLayers)
-      if(!is.null(self$soilModData$tDat$prec)){ 
+      if (!is.null(self$soilModData$tDat$prec)) { 
         self$soilModData$soilProfile$soilLayers[[1]]$wTop <- 
           self$soilModData$tDat$prec[t]
-      } # don't need else b/c default 0
-      for(i in 1:num_layers){
+      } # Else not needed b/c default set to 0
+      for (i in 1:num_layers) {
         self$soilModData$soilProfile$soilLayers[[i]] <- 
-          private$drainFCfun(self$soilModData$soilProfile$soilLayers[[i]])
-        if(i!=num_layers){
+          private$.DrainFunFC(self$soilModData$soilProfile$soilLayers[[i]])
+        if (i != num_layers) {
           self$soilModData$soilProfile$soilLayers[[i+1]]$wTop <- 
             self$soilModData$soilProfile$soilLayers[[i]]$wBot
         }
       }
     },
-    update = function(t){
-      ## module updates the t level data
+    
+    Update = function(t) {
+      # Get the number of soil layers for shorter pointer
       num_layers <- length(self$soilModData$soilProfile$soilLayers)
       self$soilModData$tDat$deepPerc[t] <- 
         self$soilModData$soilProfile$soilLayers[[num_layers]]$wBot
     }
   ),
+  
   private = list(
-    drainFCfun = function(soilLayer){
+    .DrainFunFC = function(soilLayer) {
       soilLayer$vwc <- soilLayer$vwc + soilLayer$wTop
-      if(soilLayer$vwc > soilLayer$fc){
+      if (soilLayer$vwc > soilLayer$fc) {
         soilLayer$wBot <- soilLayer$vwc - soilLayer$fc * soilLayer$depth
         soilLayer$vwc <- soilLayer$vwc - soilLayer$wBot
-      } # don't need else b/c wBot set to 0 in setup()
+      } # Else not needed b/c default set to 0 
       return(soilLayer)
     }
   )
 )
 
 
-##@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-#### DrainModuleFC Outputter ####
-##@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+# DrainModuleFC Outputter Class Generator ---------------------------
 DrainModuleFC_OP <- R6Class(
   "DrainModuleFC_OP",
   public = list(
     soilModData = NULL,
 
-    initialize = function(soilModData){
+    initialize = function(soilModData) {
       stopifnot(
-        # check if data structures exist
-        exists("tDat",soilModData),
-        exists("zDat",soilModData),
-        exists("ioPath",soilModData)
+        exists("tDat", soilModData),
+        exists("zDat", soilModData),
+        exists("ioPath", soilModData)
       ) 
       self$soilModData <- soilModData
-      
-      # make directory(s) if needed 
-      owd <- paste0(self$soilModData$ioPath,"/outputs/DrainModuleFC") # outputs working directory for module
-      if(!file.exists(owd)){ 
-        dir.create(owd)
-        dir.create(paste0(owd,"/outT"))
-      }
+      private$.MakeOutputsFolder()
     },
-    # save module specific data at t step
-    zSave_t = function(t){
+    
+    # For saving module specific data at each t step
+    Zsave_t = function(t){
       
     },
-    # plot module specific data at t step 
-    zPlot_t = function(t){
+    # For plotting module specific data at each t step 
+    Zplot_t = function(t){
       
     },
-    # plot module specific data at end of sim
-    tPlot_T = function(){
-      private$plot_dpXt()
+    # For plotting module specific data at end of sim
+    Tplots = function(){
+      private$.Plot_DPxT()
     } 
   ),
   private = list(
-    plot_dpXt = function(){
-      stopifnot(any(grepl("deepPerc",names(self$tDat))))
-      pd <- self$tDat
-      ymax <- RoundTo(max(pd$deepPerc),1,ceiling)
-      ystep <- -(ymax-0)/10
-      xstep <- (max(pd$time)-0)/10
+    .MakeOutputsFolder = function() {
+      owd <- paste0(self$soilModData$ioPath, "/outputs/DrainModuleFC") 
+      if (!file.exists(owd)) { 
+        dir.create(owd)
+        dir.create(paste0(owd, "/tOut"))
+      }
+    },
+    
+    # Plot of deep percolation x time is module specific
+    ## TODO: add in depth & time units
+    .Plot_DPxT = function(){
+      stopifnot(
+        any(grepl("deepPerc", names(self$soilModData$tDat)))
+      )
+      pd <- self$soilModData$tDat
+      ymax <- RoundTo(max(pd$deepPerc), 1, ceiling)
+      ystep <- -(ymax-0) / 10
+      xstep <- (max(pd$time) - 0) / 10
       
-      p <- ggplot(pd,
-                  aes(x=time,
-                      y=deepPerc)) +
-        geom_bar(stat="identity",
-                 color="white",
-                 fill="darkblue") +
-        scale_y_reverse(limits=c(ymax,0),
-                        labels=seq(ymax,0,ystep),
-                        breaks=seq(ymax,0,ystep)) +
+      p <- ggplot(pd, aes(x = time, y = deepPerc)) +
+        geom_bar(stat = "identity",
+                 color = "white",
+                 fill = "darkblue") +
+        scale_y_reverse(limits = c(ymax, 0),
+                        labels = seq(ymax, 0, ystep),
+                        breaks = seq(ymax, 0, ystep)) +
         scale_x_continuous(position = "top",
-                           limits=c(0.5,max(pd$time)+0.5),
-                           breaks = seq(1,max(pd$time),xstep)) +
-        labs(y="Deep Percolation",x="Time Step") +
+                           limits = c(0.5, max(pd$time) + 0.5),
+                           breaks = seq(1, max(pd$time), xstep)) +
+        labs(y = "Deep Percolation", x = "Time Step") +
         theme_classic() 
-      ## TODO: add in depth & time units
+      ggsave(filename = paste0(self$soilModData$ioPath, 
+                               "/outputs/DrainModuleFC/tOut/DPxT.png"),
+             plot = p,
+             device = "png",
+             scale = 1,
+             width = 5, 
+             height = 7.5, 
+             units = "in")
       #print(p)
-      ggsave(p,paste0(self$soilModData$ioPath,"/outputs/DrainModuleFC/outT/dpXt.png") ,
-             device = "png",scale = 1,width = 5, height = 7.5, units = "in")
     }
   )
 )
