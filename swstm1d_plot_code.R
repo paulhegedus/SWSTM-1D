5ad098894a8ee6648af5ce6f9e5c68eb32ce2127
paulhegedus <paulhegedus@montana.edu>
2020-05-28 00:59:13 -0600

    added plot code from drainModuleFC outputter

--- swstm1d_plot_code.R
+++ swstm1d_plot_code.R
@@ -98,3 +98,40 @@
 # 
 # - end
 
+
+## from SWSTM1D_OP
+# public - 
+# Plot of deep percolation x time is module specific
+## TODO: add in depth & time units
+# .Plot_DPxT = function(){
+#   stopifnot(
+#     any(grepl("deepPerc", names(self$soilModData$tDat)))
+#   )
+#   pd <- self$soilModData$tDat
+#   ymax <- RoundTo(max(pd$deepPerc), 1, ceiling)
+#   ystep <- -(ymax-0) / 10
+#   xstep <- (max(pd$time) - 0) / 10
+#   
+#   p <- ggplot(pd, aes(x = time, y = deepPerc)) +
+#     geom_bar(stat = "identity",
+#              color = "white",
+#              fill = "darkblue") +
+#     scale_y_reverse(limits = c(ymax, 0),
+#                     labels = seq(ymax, 0, ystep),
+#                     breaks = seq(ymax, 0, ystep)) +
+#     scale_x_continuous(position = "top",
+#                        limits = c(0.5, max(pd$time) + 0.5),
+#                        breaks = seq(1, max(pd$time), xstep)) +
+#     labs(y = "Deep Percolation", x = "Time Step") +
+#     theme_classic() 
+#   ggsave(filename = paste0(self$soilModData$ioPath, 
+#                            "/outputs/DrainModuleFC/tOut/DPxT.png"),
+#          plot = p,
+#          device = "png",
+#          scale = 1,
+#          width = 5, 
+#          height = 7.5, 
+#          units = "in")
+#   #print(p)
+# }
+# - end
